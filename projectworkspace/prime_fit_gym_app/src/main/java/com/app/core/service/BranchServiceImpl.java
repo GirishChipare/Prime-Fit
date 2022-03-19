fbc3cbf24d136b9b6bd04c3a14222174fe3f58e4
Girish Chipare <91129881+GirishChipare@users.noreply.github.com>
2022-03-19 15:28:27 +0530

    Added BranchServiceImpl methods

--- projectworkspace/prime_fit_gym_app/src/main/java/com/app/core/service/BranchServiceImpl.java
+++ projectworkspace/prime_fit_gym_app/src/main/java/com/app/core/service/BranchServiceImpl.java
@@ -1,11 +1,53 @@
 package com.app.core.service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.app.core.dao.GymBranchRepository;
+import com.app.core.pojos.GymBranch;
 
 @Service
 @Transactional
-public class BranchServiceImpl {
+public class BranchServiceImpl implements IBranchService{
+	
+	@Autowired
+	private GymBranchRepository branchRepo;
+
+	@Override
+	public List<GymBranch> getAllBranches() {
+		// TODO Auto-generated method stub
+		return branchRepo.findAll();
+	}
+
+	@Override
+	public GymBranch addNewBranch(GymBranch branch) {
+		// TODO Auto-generated method stub
+		return branchRepo.save(branch);
+	}
+
+	@Override
+	public GymBranch updateBranch(GymBranch branch) {
+		// TODO Auto-generated method stub
+		return branchRepo.save(branch);
+	}
+
+	@Override
+	public String deleteBranch(int branchId) {
+		// TODO Auto-generated method stub
+		branchRepo.deleteById(branchId);
+		return "Branch with Id "+branchId+" deleted successfully";
+	}
+
+	@Override
+	public GymBranch getBranchDetails(int branchId) {
+		// TODO Auto-generated method stub
+		return branchRepo.findById(branchId)
+				.orElseThrow(()->new NoSuchElementException("Branch by Id "+branchId+" not found"));
+	}
 
 }
--- projectworkspace/prime_fit_gym_app/src/main/java/com/app/core/service/IBranchService.java
+++ projectworkspace/prime_fit_gym_app/src/main/java/com/app/core/service/IBranchService.java
@@ -1,5 +1,25 @@
 package com.app.core.service;
 
+import java.util.List;
+
+import com.app.core.pojos.GymBranch;
+
 public interface IBranchService {
+	//get list of all branches
+	List<GymBranch> getAllBranches();
+	
+	//add a new branch
+	GymBranch addNewBranch(GymBranch branch);
+	
+	//update branch details
+	GymBranch updateBranch(GymBranch branch);
+	
+	//delete branch
+	String deleteBranch(int branchId);
+	
+	//get branch details by id
+	GymBranch getBranchDetails(int branchId);
+	
+	
 
 }
